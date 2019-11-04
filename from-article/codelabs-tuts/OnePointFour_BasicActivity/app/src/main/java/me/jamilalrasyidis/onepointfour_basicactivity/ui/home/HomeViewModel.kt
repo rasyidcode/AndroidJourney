4c282797dff1d610b095951878c1af54ba31c02c
jamilchan <rasyidcode7@gmail.com>
2019-11-04 21:38:12 +0700

    add codelabs tutorial

---
+++ from-article/codelabs-tuts/OnePointFour_BasicActivity/app/src/main/java/me/jamilalrasyidis/onepointfour_basicactivity/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package me.jamilalrasyidis.onepointfour_basicactivity.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
